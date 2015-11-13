ca11639c42971cead91ba86e9b607ee5fd46bc9d
gordondalos <gordondalos@gmail.com>
2015-11-13 23:41:58 +0600

    нагенерировал все страницы

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-	console.log(123456789);
+
 	$('.fa-bars').click(function () {
 		$('.hidden-menu').slideToggle(1000);
 
@@ -9,57 +9,57 @@ $(document).ready(function() {
 
 
 
-	//Цели для Яндекс.Метрики и Google Analytics
-	$(".count_element").on("click", (function() {
-		ga("send", "event", "goal", "goal");
-		yaCounterXXXXXXXX.reachGoal("goal");
-		return true;
-	}));
+	////Цели для Яндекс.Метрики и Google Analytics
+	//$(".count_element").on("click", (function() {
+	//	ga("send", "event", "goal", "goal");
+	//	yaCounterXXXXXXXX.reachGoal("goal");
+	//	return true;
+	//}));
 
 	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+	//if(!Modernizr.svg) {
+	//	$("img[src*='svg']").attr("src", function() {
+	//		return $(this).attr("src").replace(".svg", ".png");
+	//	});
+	//};
 
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				
-				$("#form").trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+	//$("#form").submit(function() {
+	//	$.ajax({
+	//		type: "POST",
+	//		url: "mail.php",
+	//		data: $(this).serialize()
+	//	}).done(function() {
+	//		alert("Спасибо за заявку!");
+	//		setTimeout(function() {
+	//
+	//			$("#form").trigger("reset");
+	//		}, 1000);
+	//	});
+	//	return false;
+	//});
 
 	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
+	//try {
+	//	$.browserSelector();
+	//	if($("html").hasClass("chrome")) {
+	//		$.smoothScroll();
+	//	}
+	//} catch(err) {
+    //
+	//};
 
-	};
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
+	//$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+//
 });
 
-$(window).load(function() {
-
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-
-});
+//$(window).load(function() {
+//
+//	$(".loader_inner").fadeOut();
+//	$(".loader").delay(400).fadeOut("slow");
+//
+//});
 
 
 

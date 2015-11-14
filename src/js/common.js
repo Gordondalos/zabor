e67c075e0147f63b094cbea7086908c39596c063
gordondalos <gordondalos@gmail.com>
2015-11-14 18:37:08 +0600

    реализовал расчет забора из профнастила

--- src/js/common.js
+++ src/js/common.js
@@ -8,6 +8,78 @@ $(document).ready(function() {
 	});
 
 
+	//Расчет профнастила
+
+
+
+	$('#profnastil').submit(function(e){
+		e.preventDefault();
+		submitaction();
+	});
+
+
+	function submitaction(){
+
+		$('#add').remove();
+		var length = $('#length').val(); // длина забора
+
+		if(length)
+		{
+			var step = $('#step').val(); // количетсво столбов
+			var higth = $('#higth').val(); // высота забора
+			var material = $('#material').val(); // цена материла за м2
+			var lagicol = $('#lagicol').val(); // количество лаг
+			var stolb = $('#stolb').val(); // цена материала за столб 1м
+			var lagid = $('#lagid').val(); // цена лаги за 1м от материала
+			var vorota = $('#vorota').val(); // количетство ворот
+			var kalitka = $('#kalitka').val(); // количество калиток
+
+			var stroimostvorot = 50000; // стоимость ворот
+			var stroimostkalitki = 10000; // строимость калитки
+
+			var cenastolbov = (length/step+1)*stolb*higth; // цена столбов
+			var cenalag = (length*lagicol)*lagid; // цена лаг
+			var cenasten = (length*higth)*material; // цена материала
+			var cenavorot = vorota*stroimostvorot; // цена вотор
+			var cenakalitki = kalitka*stroimostkalitki; // цена калитки
+			var cenamateriala = cenastolbov+cenalag+cenasten+cenavorot+cenakalitki; // общая цена маетирала
+			var cenarabot = cenamateriala*1.30; // цена работ на 30% дороже стоисоти материалов коэфициент
+			var itogo = cenamateriala+cenarabot; // итого
+
+			itogo = itogo.toFixed(2);
+			$('#profnastil').append("<span id='add'>стоимоть вашего забора: "+itogo+"рублей</span>");
+
+		}
+		else{
+
+			$('#profnastil').append("<span id='add'>Цена забора обязательный параметр</span>");
+
+
+		}
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	////Цели для Яндекс.Метрики и Google Analytics
 	//$(".count_element").on("click", (function() {

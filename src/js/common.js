4dc495fff376bab033cc4bd6ce2bca1a049f25b2
gordondalos <Gordondalos@gmail.com>
2016-06-12 16:34:14 +0600

    Заливка на гитхаб

--- src/js/common.js
+++ src/js/common.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 		if(length)
 		{
 			var step = $('#step').val(); // количетсво столбов
-			var higth = $('#higth').val(); // высота забора
+			var higth = $('#higth').val(); // высота забора в м
 			var material = $('#material').val(); // цена материла за м2
 			var lagicol = $('#lagicol').val(); // количество лаг
 			var stolb = $('#stolb').val(); // цена материала за столб 1м
@@ -37,10 +37,10 @@ $(document).ready(function() {
 			var stroimostvorot = 50000; // стоимость ворот
 			var stroimostkalitki = 10000; // строимость калитки
 
-			var cenastolbov = (length/step+1)*stolb*higth; // цена столбов
+			var cenastolbov = (length/step+1)*stolb*higth+ 0.6; // цена столбов
 			var cenalag = (length*lagicol)*lagid; // цена лаг
 			var cenasten = (length*higth)*material; // цена материала
-			var cenavorot = vorota*stroimostvorot; // цена вотор
+			var cenavorot = vorota*stroimostvorot; // цена ворот
 			var cenakalitki = kalitka*stroimostkalitki; // цена калитки
 			var cenamateriala = cenastolbov+cenalag+cenasten+cenavorot+cenakalitki; // общая цена маетирала
 			var cenarabot = cenamateriala*1.30; // цена работ на 30% дороже стоисоти материалов коэфициент
